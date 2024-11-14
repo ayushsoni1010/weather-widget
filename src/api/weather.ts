04f27a33b2b32472e1cc8a27a13eb0e1d9fa4863
Ayush Soni <ayushsoni1010.work@gmail.com>
2024-11-14 23:42:52 +0530

    feat: created api for getting coordinates

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,9 +1,19 @@
 import { WEATHER_API_KEY, WEATHER_API_URL } from "@/config/vars";
 import axios from "axios";
 
-export const getWeatherByLocation = async (q: string): Promise<WeatherType> => {
+export const getWeatherByLocation = async (q: string) => {
   const respone = await axios.get<WeatherType>(
     `${WEATHER_API_URL}?q=${q}&appid=${WEATHER_API_KEY}`
+  );
+  return respone.data;
+};
+
+export const getWeatherByCoordinates = async (
+  latitude: number,
+  longitude: number
+) => {
+  const respone = await axios.get<WeatherType>(
+    `${WEATHER_API_URL}?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
   );
   return respone.data;
 };
